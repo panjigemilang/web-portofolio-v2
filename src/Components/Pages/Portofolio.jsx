d85e490991b85c0edeeb44ba3c917a41bd209c94
panjigemilang <41560479+panjigemilang@users.noreply.github.com>
2020-09-12 20:22:02 +0700

    add portofolio onscroll animation

--- src/Components/Pages/Portofolio.jsx
+++ src/Components/Pages/Portofolio.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import Index from "../../Context"
 import Card from "./SubPages/Card"
 import { content2019, content2020 } from "../Utils/ContentVariables"
-import { useScrollPosition } from "../Utils/useScrollPosition"
 import "./portofolio.scss"
 
 export default function Portofolio() {
@@ -13,27 +12,6 @@ export default function Portofolio() {
     setToggleLoading,
   } = React.useContext(Index)
   const [content, setContent] = React.useState([])
-  let imgElements
-
-  useScrollPosition(({ prevPos, currPos }) => {
-    const y = currPos.y * -1
-    imgElements = document.querySelectorAll(".card-app")
-    console.log("Position", y)
-
-    if (y < 200) {
-      for (let i = 2; i < imgElements.length; i++) {
-        imgElements[i].classList.remove("show")
-      }
-    } else if (y >= 200 && y < 750) {
-      for (let i = 2; i < imgElements.length - 2; i++) {
-        imgElements[i].classList.add("show")
-      }
-    } else if (y >= 750) {
-      for (let i = 4; i < imgElements.length; i++) {
-        imgElements[i].classList.add("show")
-      }
-    }
-  })
 
   React.useEffect(() => {
     window.scrollTo(0, 0)
@@ -62,9 +40,11 @@ export default function Portofolio() {
             </div>
             {content.map((item, i) => (
               <div
-                className={`card-app ${i > 1 && i % 2 !== 0 ? "right" : ""}${
-                  i > 1 && i % 2 === 0 ? "left" : ""
-                }`}
+                className="card-app"
+                data-aos={i % 2 !== 0 ? "zoom-in-left" : "zoom-in-right"}
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
                 key={i}
               >
                 <Card item={item} />
